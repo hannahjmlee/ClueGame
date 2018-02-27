8fde7117f24c1a7852afb9b2230d5de3deda0ace
Hannah Lee <hannahlee@mines.edu>
2018-02-27 11:32:16 -0700

    added empty functions to IntBoard class

--- src/experiment/BoardCell.java
+++ src/experiment/BoardCell.java
@@ -1,5 +1,7 @@
 package experiment;
 
 public class BoardCell {
-
+	private int row; 
+	private int column; 
+	
 }
--- src/experiment/IntBoard.java
+++ src/experiment/IntBoard.java
@@ -1,5 +1,33 @@
 package experiment;
 
-public class IntBoard {
+import java.util.*;
 
+public class IntBoard {
+	private BoardCell [][] grid;
+	private Map <BoardCell, Set<BoardCell>> adjMtx; 
+	private Set <BoardCell> visited; 
+	private Set <BoardCell> targets; 
+	
+	public IntBoard(){
+		grid = new BoardCell[4][4];
+		calcAdjencies(); 
+	}
+	
+	
+	public void calcAdjencies(){
+		return;
+	}
+	
+	public Set<BoardCell> getAdjList(){
+		return null;  
+	}
+	
+	public List calcTargets(int startCell, int pathLength){
+		return null;
+		
+	}
+	
+	public Set <BoardCell> getTargets(){
+		return null; 
+	}
 }
