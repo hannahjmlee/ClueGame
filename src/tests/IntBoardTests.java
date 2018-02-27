f3febff9249e4d523e57dc0187f45e39fcc7690e
Hannah Lee <hannahlee@mines.edu>
2018-02-27 11:40:26 -0700

    added empty test functions to IntBoardTests Junit class

--- src/tests/IntBoardTests.java
+++ src/tests/IntBoardTests.java
@@ -1,13 +1,52 @@
 package tests;
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import experiment.IntBoard;
+
 public class IntBoardTests {
-
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	private IntBoard board; 
+	
+	@Before
+	public void beforeAll(){
+		board = new IntBoard(); 
 	}
-
+	
+	@Test
+	public void topLeft(){
+		
+	}
+	
+	@Test
+	public void bottomRight(){
+		
+	}
+	
+	@Test
+	public void rightEdge(){
+		
+	}
+	
+	@Test
+	public void leftEdge(){
+		
+	}
+	
+	@Test
+	public void secondColumnMiddle(){
+		
+	}
+	
+	@Test
+	public void secondLastColumnMiddle(){
+		
+	}
+	
+	@Test
+	public void testTargets(){
+		
+	}
+	
 }
