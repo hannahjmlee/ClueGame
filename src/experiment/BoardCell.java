07bda12b33b387b54d0fd179fd68b832da03f3b7
Hannah Lee <hannahlee@mymail.mines.edu>
2018-02-28 12:48:49 -0700

    added comments to remaining files. -CONCLUDES PART 1-

--- src/experiment/BoardCell.java
+++ src/experiment/BoardCell.java
@@ -1,5 +1,12 @@
 package experiment;
 
+/**
+ * BoardCell class -- characterized by a row and a column
+ * 
+ * @author Hannah Lee
+ * @author Savannah Paul
+ *
+ */
 public class BoardCell {
 	private int row; 
 	private int column; 
--- src/experiment/IntBoard.java
+++ src/experiment/IntBoard.java
@@ -1,5 +1,11 @@
+/**
+ * IntBoard -- creates a board comprised of BoardCells and contains all methods
+ * necessary for the game to run correctly. 
+ * 
+ * @author Hannah Lee
+ * @author Savannah Paul
+ */
 package experiment;
-
 import java.util.*;
 
 public class IntBoard {
@@ -8,29 +14,55 @@ public class IntBoard {
 	private Set <BoardCell> visited; 
 	private Set <BoardCell> targets; 
 	
+	/**
+	 * IntBoard constructor -- creates a 4x4 board and creates a adjacency map. 
+	 */
 	public IntBoard(){
 		grid = new BoardCell[4][4];
 		calcAdjencies(); 
 	}
 	
-	
+	/**
+	 * calcAdjencies -- populates adjacency map for each position on the board
+	 */
 	public void calcAdjencies(){
 		return;
 	}
 	
+	/**
+	 * getAdjList -- returns adjacency list at a certain position by accessing the values
+	 * in adjMtx.
+	 * @param c is the specific position we are pulling the adjacency list from
+	 * @return adjacency list for board cell c
+	 */
 	public Set<BoardCell> getAdjList(BoardCell c){
 		return null;  
 	}
 	
-	public List calcTargets(BoardCell startCell, int pathLength){
-		return null;
-		
+	/**
+	 * calcTargets -- calculates a list of all possible target positions
+	 * @param startCell the starting position of the player
+	 * @param pathLength the number they rolled on the dice / the number of spaces they are allowed to move
+	 * @return a list that contains all possible target positions
+	 */
+	public List<BoardCell> calcTargets(BoardCell startCell, int pathLength){
+		return null;	
 	}
 	
+	/**
+	 * getTargets -- getter that returns the list of possible target positions
+	 * @return targets the set that contains all possible target cells
+	 */
 	public Set <BoardCell> getTargets(){
 		return null; 
 	}
 	
+	/**
+	 * getCell -- getter that returns type BoardCell at the location provided
+	 * @param row that the cell is at
+	 * @param column that the cell is at
+	 * @return the BoardCell at position (row, column)
+	 */
 	public BoardCell getCell(int row, int column){
 		return null;
 	}
