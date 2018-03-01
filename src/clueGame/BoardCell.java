c782cb999e6542372686f0e92c0e436eff585312
Hannah Lee <hannahlee@mines.edu>
2018-03-01 11:28:59 -0700

    added class stubs and uploaded test configurations

---
+++ src/clueGame/BoardCell.java
@@ -0,0 +1,30 @@
+package clueGame;
+
+/**
+ * BoardCell class -- characterized by a row and a column
+ * 
+ * @author Hannah Lee
+ * @author Savannah Paul
+ *
+ */
+public class BoardCell {
+	private int row; 
+	private int column; 
+	
+	public BoardCell(int r, int c) {
+		row = r; 
+		column = c; 
+	}
+	
+	public int getRow(){
+		return row;
+	}
+	public int getCol(){
+		return column;
+	}
+
+	@Override
+	public String toString() {
+		return "BoardCell [row=" + row + ", column=" + column + "]";
+	} 
+}
