d48fcd3b4c70a7f079c0cc614d5e9cf3b144ed62
Hannah Lee <hannahlee@mymail.mines.edu>
2018-03-29 18:58:46 -0600

    created gameSetupTests

---
+++ src/tests/gameSetupTests.java
@@ -0,0 +1,19 @@
+package tests;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+import clueGame.Board;
+
+public class gameSetupTests {
+	
+	@BeforeClass
+	public static void setUp() {
+	}
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
